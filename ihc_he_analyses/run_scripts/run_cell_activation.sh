3f8fd12ac8a8c4d731a4e8fc0f7a5fd841c47784
minajamshidi <minajamshidi91@gmail.com>
2025-11-19 12:09:20 +0100

    run scripts

---
+++ ihc_he_analyses/run_scripts/run_cell_activation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+patient_id=P6
+marker=pSTAT3
+activation_marker='Nucleus: DAB OD mean'
+
+zenodo_sample_dir=/home/space/oncology/multimodal-fusion/data/lmu/zenodo_sample
+tiles_dirs=( "$zenodo_sample_dir"/patches )
+reg_dir="$zenodo_sample_dir"/results/registration
+qupath_measurements_dir="$zenodo_sample_dir"/qupath_measurements
+predictions_dirs=( /home/space/oncology/multimodal-fusion/data/lmu/zenodo_sample/predictions )
+results_dir="$zenodo_sample_dir"/results/ihc_he_analysis
+mkdir -p $results_dir
+
+
+python3 cell_activation_analyze.py \
+--tiles-dirs "${tiles_dirs[@]}" \
+--registration-dir $reg_dir \
+--qupath-measurement-dir $qupath_measurements_dir \
+--predictions-dirs "${predictions_dirs[@]}" \
+--results-dir $results_dir \
+--marker "$marker" \
+--activation-marker "$activation_marker" \
+--patient-id "$patient_id" \
+--overwrite-results
